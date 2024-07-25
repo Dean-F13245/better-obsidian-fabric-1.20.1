5cae571eafd68ce6121c2a6f820c28bcec1533d5
dean-f13245~ <deanfarrelly00@gmail.com~>
2024-07-25 03:15:44 +0100

    Fixed Reinforced obsidian not dropping when mined

--- src/main/java/net/dean/betterobsidian/Block/custom/new_obsidian.java
+++
@@ -1,9 +0,0 @@
-package net.dean.betterobsidian.Block.custom;
-
-import net.minecraft.block.Block;
-
-public class new_obsidian extends Block {
-    public new_obsidian(Settings settings){
-        super(settings);
-    }
-}
